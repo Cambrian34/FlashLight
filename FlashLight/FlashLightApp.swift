dd95d87aff97f1f3f7bcfbdd8ee141f3b1ac3ec6
Alistair Chambers <alicham6@gmail.com>
2023-12-15 13:12:38 -0600

    Initial Commit

---
+++ FlashLight/FlashLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashLightApp.swift
+//  FlashLight
+//
+//  Created by Alistair Chambers on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
